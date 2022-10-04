21656b28a765602f68c100f0eee640aedde81a8f
geffk2 <sluxx@icloud.com>
2022-10-04 21:07:56 +0300

    Week 5 done for real now

--- week05/ex4.c
+++ week05/ex4.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define true 1
-#define false 0
-
 int result = 0;
-pthread_mutex_t mut;
+int nextNumber = 0;
+
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 
 int is_prime(int n) {
   if (n <= 1)
@@ -17,28 +16,27 @@ int is_prime(int n) {
   return 1;
 }
 
-int primes_count_in_interval(int start, int finish) {
-  int ret = 0;
-  for (int i = start; i < finish; i++)
-    if (is_prime(i) != 0)
-      ret++;
-  return ret;
-}
-
-typedef struct prime_counter_request
-{
-  int start, finish;
-} Req;
 
 void* thread(void* arg) {
-  Req* req = (Req*) arg;
-  int cnt = primes_count_in_interval(req->start, req->finish);
-  free(req);
+  int* lim = (int*) arg;
+  int cnt = 0;
+  int currentNum;
+
+  for(;;) {
+    pthread_mutex_lock(&mut);
+    currentNum = nextNumber++;
+    pthread_mutex_unlock(&mut);
+    if(currentNum >= *lim) {
+      break;
+    }
+    cnt += is_prime(currentNum);
+  }
+
   pthread_mutex_lock(&mut);
   result += cnt;
   pthread_mutex_unlock(&mut);
 
-  return NULL;
+  pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
@@ -48,19 +46,10 @@ int main(int argc, char *argv[]) {
   int n, m;
   sscanf(argv[1], "%d", &n);
   sscanf(argv[2], "%d", &m);
-  int per_process = n / m;
   pthread_t* threads = calloc(m, sizeof(pthread_t)); 
 
-  pthread_mutex_init(&mut, NULL);
   for(int i = 0; i < m; i++) {
-    int start = i * per_process;
-    int end = i + 1 < m ? (i + 1) * per_process : n; 
-
-    Req* req = malloc(sizeof(Req));
-    req->start = start;
-    req->finish = end;
-
-    pthread_create(threads + i, NULL, &thread, req);
+    pthread_create(threads + i, NULL, &thread, &n);
   }
 
   for(int i = 0; i < m; i++) {
@@ -69,6 +58,7 @@ int main(int argc, char *argv[]) {
   printf("%d\n", result);
 
   free(threads);
+
   pthread_mutex_destroy(&mut);
   return 0;
 }
